2b79b1b022a5fe7bb6f627258a8fb8e90282cd3c
GhostEntity12 <48895274+GhostEntity12@users.noreply.github.com>
2022-02-26 19:44:36 +1100

    UI stuff

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : Singleton<GameManager>
 {
+	[Flags] public enum PickupsFlag { Nothing = 0, Blood = 1, Footprint = 2, Drawings = 4, Hair = 8, Everything = 15 }
 	public GameObject player;
     public int textboxDistance = 50;
 	public float textboxBuffer = 3;
 	public CanvasGroup canvas;
 	public bool canMove = true;
+
+	PickupsFlag pickups;
+	public bool AllPickups => pickups.HasFlag(PickupsFlag.Everything);
+
+	public Image[] pickupImages = new Image[4];
+	public Sprite[] pickupSprites = new Sprite[4];
 
 	[HideInInspector]
     new public Camera camera;
@@ -23,5 +32,12 @@ public class GameManager : Singleton<GameManager>
 		camera = Camera.main;
 		textboxPool = FindObjectOfType<TextboxPool>();
 		player = FindObjectOfType<Player>().gameObject;
+	}
+
+	public void SetFlag(PickupsFlag flag)
+	{
+		pickups |= flag;
+		int index = (int)Mathf.Log((int)flag, 2);
+		pickupImages[index].sprite = pickupSprites[index];
 	}
 }
--- Assets/Scripts/PromptPickup.cs
+++ Assets/Scripts/PromptPickup.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PromptPickup : Prompt
 {
 	bool disabled;
+	public GameManager.PickupsFlag type;
 
 	// Start is called before the first frame update
 	protected override void Awake()
@@ -20,7 +21,7 @@ public class PromptPickup : Prompt
 		if (!disabled && active && Input.GetKeyDown(key))
 		{
 
-			Debug.Log("Picked Up");
+			GameManager.Instance.SetFlag(type);
 			disabled = true;
 			state = State.Shrinking;
 		}
